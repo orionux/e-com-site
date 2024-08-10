da02a07bb75810966f330dca9a10e02b64c3ea41
tj <thinupavani@gmail.com>
2024-08-10 14:21:38 +0530

    about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -21,160 +21,34 @@ const page = () => {
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="story-img">
-                            <img src="assets/img/banner/11.png" alt=""></img>
-                            <div className="about-logo">
+                            <img src="assets/img/vision.svg" alt=""></img>
+                            {/* <div className="about-logo">
                                 <h3>ezone</h3>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="story-details pl-50">
                             <div className="story-details-top">
-                                <h2>WELCOME TO <span>ezone</span></h2>
-                                <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a complete account of the system, and expound the actual teachings of the eat explorer. </p>
-                            </div>
-                            <div className="story-details-bottom">
-                                <h4>WE START AT 2015</h4>
-                                <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a complete account of the system, and expound the actual teachings of the eat explorer.</p>
-                            </div>
-                            <div className="story-details-bottom">
-                                <h4>WIN BEST ONLINE SHOP AT 2022</h4>
-                                <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a complete account of the system, and expound the actual teachings of the eat explorer. </p>
+                                <h2>Vision<span></span></h2>
+                                <p>We aspire to be a leading provider of high-quality tobacco accessories that enhance the enjoyment of tobacco products for our customers. Our vision is to create a community of individuals who appreciate the art of tobacco smoking and respect the traditions associated with it. We want to be known for our commitment to quality, innovation, and exceptional customer service.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div className="about-banner-area pb-65">
-            <div className="container">
+
                 <div className="row">
-                    <div className="col-lg-4">
-                        <div className="banner-wrapper-4 mb-30">
-                            <a href="#"><img src="assets/img/banner/37.jpg" alt=""></img></a>
-                            <div className="banner-content4-style1">
-                                <h4>Best <br></br>Electronics <br></br>Products.</h4>
-                            </div>
+                    
+                    <div className="col-lg-6">
+                        <div className="story-details pl-50">
+                        <h2>Mission<span></span></h2>
+                        <p>Our mission is to offer a diverse range of tobacco accessories that cater to the needs of our customers. We provide a selection of pipes, cigar cutters, lighters, humidors, and other products that are made with the highest quality materials and craftsmanship. Our products are carefully selected to ensure that they meet our rigorous standards for functionality, durability, and aesthetics. We are committed to providing exceptional customer service and maintaining high-quality standards for all our products. Our team of experts is always available to help customers choose the right accessories that suit their preferences and enhance their tobacco smoking experience. We are dedicated to creating an environment that promotes responsible tobacco use and respect for all individuals. We believe that tobacco smoking is an art form that requires skill, knowledge, and patience. We aim to help our customers appreciate the nuances of tobacco smoking and develop a deeper understanding of its history and cultural significance. In summary, our mission is to help our customers enhance their tobacco smoking experiences with the right tools and accessories. We are passionate about what we do and strive to provide the highest level of quality, service, and innovation in all that we do.</p>
                         </div>
                     </div>
-                    <div className="col-lg-4">
-                        <div className="banner-wrapper-4 mb-30">
-                            <a href="#"><img src="assets/img/banner/21.jpg" alt=""></img></a>
-                            <div className="banner-content4-style2">
-                                <h5 className="p-left">get</h5>
-                                <h2>25% </h2>
-                                <h5 className="p-right">off</h5>
-                                <h3>Bitso X1202</h3>
-                            </div>
+                    <div className="col-lg-6">
+                        <div className="story-img">
+                            <img src="assets/img/mission.svg" alt=""></img>
                         </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="banner-wrapper-4 mb-30">
-                            <a href="#"><img src="assets/img/banner/22.jpg" alt=""></img></a>
-                            <div className="banner-content4-style3">
-                                <h1>Up to <br></br>10% Off</h1>
-                                <h3>Lonovo Vio D22</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="goal-area pb-65">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <div className="goal-wrapper mb-30">
-                            <h3>OUR VISSION</h3>
-                            <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs of the eat explorer of the truth.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="goal-wrapper mb-30">
-                            <h3>OUR MISSION</h3>
-                            <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs of the eat explorer of the truth.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <div className="goal-wrapper mb-30">
-                            <h3>OUR GOAL</h3>
-                            <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born an will give you a ete account of the system, and expound the actual teangs of the eat explorer of the truth.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="choose-area pb-100">
-            <div className="container">
-                <div className="about-section">
-                    <h3>YOU CAN CHOOSE US BECAUSE <br></br>WE ALWAYS PROVIDE IMPORTANCE...</h3>
-                    <p>ezone provide how all this mistaken idea of denouncing pleasure and sing pain was born will give                            you a complete account of the system, and expound the actual </p>
-                </div>
-                <div className="row">
-                    <div className="col-lg-7 col-md-12">
-                        <div className="all-causes">
-                            <div className="row">
-                                <div className="col-md-6 causes-res">
-                                    <div className="choose-wrapper">
-                                        <h4>FAST DELIVERY</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                    <div className="choose-wrapper">
-                                        <h4>SECURE PAYMENT</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                    <div className="choose-wrapper">
-                                        <h4>EASY ORDER TRACKING</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                    <div className="choose-wrapper">
-                                        <h4>24/7 SUPPORT</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="choose-wrapper">
-                                        <h4>QUALITY PRODUCT</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                    <div className="choose-wrapper">
-                                        <h4>MONEY BACK GUARNTEE</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                    <div className="choose-wrapper">
-                                        <h4>FREE RETURN</h4>
-                                        <p>ezone provide how all this mistaken dea of denouncing pleasure and sing </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-5 col-md-12">
-                        <div className="choose-banner-wrapper f-right">
-                            <img src="assets/img/banner/38.jpg" alt=""></img>
-                            <div className="choose-banner-text">
-                                <h4>DEALS <br></br>OF THE DAY</h4>
-                                <h3>UP TO <br></br><span>50%</span> <br></br>OFF</h3>
-                                <a href="#">SHOP NOW </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="team-area">
-            <img src="assets/img/banner/39.jpg" alt=""></img>
-        </div>
-        <div className="testimonials-area pt-100 pb-95 bg-img" style={{backgroundImage: "url(assets/img/bg/7.jpg)"}}>
-            <div className="container">
-                <div className="testimonials-active owl-carousel">
-                    <div className="single-testimonial-2 text-center">
-                        <img src="assets/img/team/1.png" alt=""></img>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        <img src="assets/img/team/2.png" alt=""></img>
-                        <h4>tayeb rayed</h4>
-                        <span>uiux Designer</span>
                     </div>
                 </div>
             </div>
