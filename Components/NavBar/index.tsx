20badfecf6c90f9f788c49b2d21cce9b137412a9
tj <thinupavani@gmail.com>
2024-09-11 21:32:55 +0530

    logout button added

--- Components/NavBar/index.tsx
+++ Components/NavBar/index.tsx
@@ -13,6 +13,8 @@ type IconType = 'order' | 'product' | 'fav';
 
 const NavBar = () => {
   const pathname = usePathname();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
 
   const [activeIcon, setActiveIcon] = useState<IconType | null>(null);
 
@@ -25,17 +27,32 @@ const NavBar = () => {
       setActiveIcon('fav');
     }
   }, [pathname]);
-  
+
   const handleIconClick = (icon: IconType) => {
     setActiveIcon(icon);
   };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('authToken');
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }
+}, []);
+
+const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    setIsLoggedIn(false);
+    window.location.href = '/';
+};
 
   return (
     <div>
 
       <div className="header-top-furniture wrapper-padding-2 res-header-sm">
         <div className="container-fluid">
-          <div className="header-bottom-wrapper" style={{position:'relative'}}>
+          <div className="header-bottom-wrapper" style={{ position: 'relative' }}>
             <div className="logo-2 furniture-logo ptb-30">
               <a href="index.html">
                 <img src="/assets/img/svg/ferolislogo.svg" width="180" height="50" alt="" />
@@ -46,57 +63,15 @@ const NavBar = () => {
                 <ul className={`${styles.navbar}`}>
                   <li>
                     <a href="/" className={`${pathname === '/' ? styles.active : ''}`}>home</a>
-                    {/* <ul className="single-dropdown">
-                      <li>
-                        <a href="index.html">Fashion</a>
-                      </li>
-                      <li>
-                        <a href="index-fashion-2.html">Fashion style 2</a>
-                      </li>
-                      <li>
-                        <a href="index-fruits.html">fruits</a>
-                      </li>
-                      <li>
-                        <a href="index-book.html">book</a>
-                      </li>
-                      <li>
-                        <a href="index-electronics.html">electronics</a>
-                      </li>
-                      <li>
-                        <a href="index-electronics-2.html">
-                          electronics style 2
-                        </a>
-                      </li>
-                      <li>
-                        <a href="index-food.html">food & drink</a>
-                      </li>
-                      <li>
-                        <a href="/">furniture</a>
-                      </li>
-                      <li>
-                        <a href="index-handicraft.html">handicraft</a>
-                      </li>
-                      <li>
-                        <a target="_blank" href="index-smart-watch.html">
-                          smart watch
-                        </a>
-                      </li>
-                      <li>
-                        <a href="index-sports.html">sports</a>
-                      </li>
-                    </ul> */}
                   </li>
                   <li>
                     <a href="/product" className={`${pathname === '/product' ? styles.active : ''}`}>Product</a>
                   </li>
-                  {/* <li>
-                    <a href="/cart">Cart</a>
-                  </li> */}
                   <li>
                     <a href="/about-us" className={`${pathname === '/about-us' ? styles.active : ''}`}>About Us</a>
                   </li>
                   <li>
-                    <a href="/privacy-policy" className={`${pathname === '/privacy-policy' ? styles.active : '' }`}>privacy policy</a>
+                    <a href="/privacy-policy" className={`${pathname === '/privacy-policy' ? styles.active : ''}`}>privacy policy</a>
                   </li>
                   <li>
                     <a href="/contact" className={`${pathname === '/contact' ? styles.active : ''}`}>contact</a>
@@ -105,137 +80,59 @@ const NavBar = () => {
               </nav>
             </div>
             <div className="header-cart d-flex align-items-center">
-            <div className="furniture-login">
-              <ul>
+              <div className="furniture-login">
+                <ul>
+                {!isLoggedIn ? (
+                <>
+                    <li>
+                        <a href="/signin">Login</a>
+                    </li>
+                    <li>
+                        <a href="/register">Register</a>
+                    </li>
+                </>
+            ) : (
                 <li>
-                  <a href="/signin">Login </a>
+                    <a href="#" onClick={handleLogout}>Logout</a>
                 </li>
-                <li>
-                  <a href="/register">Reg </a>
-                </li>
-              </ul>
+            )}
+                </ul>
+              </div>
             </div>
+           
+
+            <div className={`${styles.heroBarParent} `}>
+              <div className={`${styles.heroBarSection} `}>
+                <div className={styles.heroBar}>
+                  <div
+                    className={`${styles.orderIcon} ${activeIcon === 'order' ? styles.activeBtn : ''}`}
+                    onClick={() => handleIconClick('order')}
+                  >
+                    <a href="/product">
+                      <OrderIcon height={20} width={20} stroke={activeIcon === 'order' ? "#5f6b6e" : "#fff"} />
+                    </a>
+                  </div>
+                  <div
+                    className={`${styles.productIcon} ${activeIcon === 'product' ? styles.activeBtn : ''}`}
+                    onClick={() => handleIconClick('product')}
+                  >
+                    <a href="/cart">
+                      <ProductIcon width={20} height={20} stroke={activeIcon === 'product' ? "#5f6b6e" : "#fff"} />
+                    </a>
+                  </div>
+                  <div
+                    className={`${styles.favIcon} ${activeIcon === 'fav' ? styles.activeBtn : ''}`}
+                    onClick={() => handleIconClick('fav')} >
+                    <a href="/favProducts">
+                      <FavIcon width={20} height={20} fill={activeIcon === 'fav' ? "#5f6b6e" : "#fff"} />
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-            {/* <div className="header-cart">
-              <a className="icon-cart-furniture" href="#">
-                <i className="ti-shopping-cart"></i>
-                <span className="shop-count-furniture green">02</span>
-              </a>
-              <ul className="cart-dropdown">
-                <li className="single-product-cart">
-                  <div className="cart-img">
-                    <a href="#">
-                      <img src="assets/img/cart/1.jpg" alt="" />
-                    </a>
-                  </div>
-                  <div className="cart-title">
-                    <h5>
-                      <a href="#"> Bits Headphone</a>
-                    </h5>
-                    <h6>
-                      <a href="#">Black</a>
-                    </h6>
-                    <span>$80.00 x 1</span>
-                  </div>
-                  <div className="cart-delete">
-                    <a href="#">
-                      <i className="ti-trash"></i>
-                    </a>
-                  </div>
-                </li>
-                <li className="single-product-cart">
-                  <div className="cart-img">
-                    <a href="#">
-                      <img src="assets/img/cart/2.jpg" alt="" />
-                    </a>
-                  </div>
-                  <div className="cart-title">
-                    <h5>
-                      <a href="#"> Bits Headphone</a>
-                    </h5>
-                    <h6>
-                      <a href="#">Black</a>
-                    </h6>
-                    <span>$80.00 x 1</span>
-                  </div>
-                  <div className="cart-delete">
-                    <a href="#">
-                      <i className="ti-trash"></i>
-                    </a>
-                  </div>
-                </li>
-                <li className="single-product-cart">
-                  <div className="cart-img">
-                    <a href="#">
-                      <img src="assets/img/cart/3.jpg" alt="" />
-                    </a>
-                  </div>
-                  <div className="cart-title">
-                    <h5>
-                      <a href="#"> Bits Headphone</a>
-                    </h5>
-                    <h6>
-                      <a href="#">Black</a>
-                    </h6>
-                    <span>$80.00 x 1</span>
-                  </div>
-                  <div className="cart-delete">
-                    <a href="#">
-                      <i className="ti-trash"></i>
-                    </a>
-                  </div>
-                </li>
-                <li className="cart-space">
-                  <div className="cart-sub">
-                    <h4>Subtotal</h4>
-                  </div>
-                  <div className="cart-price">
-                    <h4>$240.00</h4>
-                  </div>
-                </li>
-                <li className="cart-btn-wrapper">
-                  <a className="cart-btn btn-hover" href="#">
-                    view cart
-                  </a>
-                  <a className="cart-btn btn-hover" href="#">
-                    checkout
-                  </a>
-                </li>
-              </ul>
-            </div> */}
-          
-          <div className={`${styles.heroBarParent} `}>
-                        <div className={`${styles.heroBarSection} `}>
-                            <div className={styles.heroBar}>
-                                <div
-                                    className={`${styles.orderIcon} ${activeIcon === 'order' ? styles.activeBtn : ''}`}
-                                    onClick={() => handleIconClick('order')} 
-                                >
-                                    <a href="/product">
-                                    <OrderIcon height={20} width={20} stroke={activeIcon === 'order' ? "#5f6b6e" : "#fff"} />
-                                    </a>
-                                </div>
-                                <div
-                                    className={`${styles.productIcon} ${activeIcon === 'product' ? styles.activeBtn : ''}`}
-                                    onClick={() => handleIconClick('product')}
-                                >
-                                    <a href="/cart">
-                                    <ProductIcon width={20} height={20} stroke={activeIcon === 'product' ? "#5f6b6e" : "#fff"} />
-                                    </a>
-                                </div>
-                                <div
-                                    className={`${styles.favIcon} ${activeIcon === 'fav' ? styles.activeBtn : ''}`}
-                                    onClick={() => handleIconClick('fav')} >
-                                    <a href="/favProducts">
-                                    <FavIcon width={20} height={20} fill={activeIcon === 'fav' ? "#5f6b6e" : "#fff"} />
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        </div> 
 
           </div>
-          
+
           <div className="row">
             <div className="mobile-menu-area d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
               <div className="mobile-menu">
@@ -243,58 +140,20 @@ const NavBar = () => {
                   <ul className="menu-overflow">
                     <li>
                       <a href="/">HOME</a>
-                      {/* <ul>
-                        <li>
-                          <a href="index.html">Fashion</a>
-                        </li>
-                        <li>
-                          <a href="index-fashion-2.html">Fashion style 2</a>
-                        </li>
-                        <li>
-                          <a href="index-fruits.html">Fruits</a>
-                        </li>
-                        <li>
-                          <a href="index-book.html">book</a>
-                        </li>
-                        <li>
-                          <a href="index-electronics.html">electronics</a>
-                        </li>
-                        <li>
-                          <a href="index-electronics-2.html">
-                            electronics style 2
-                          </a>
-                        </li>
-                        <li>
-                          <a href="index-food.html">food & drink</a>
-                        </li>
-                        <li>
-                          <a href="index-furniture.html">furniture</a>
-                        </li>
-                        <li>
-                          <a href="index-handicraft.html">handicraft</a>
-                        </li>
-                        <li>
-                          <a href="index-smart-watch.html">smart watch</a>
-                        </li>
-                        <li>
-                          <a href="index-sports.html">sports</a>
-                        </li>
-                      </ul> */}
+                     
                     </li>
                     <li>
-                    <a href="/product">Product</a>
+                      <a href="/product">Product</a>
                     </li>
-                    {/* <li>
-                    <a href="/cart">Cart</a>
-                    </li> */}
+                    
                     <li>
-                    <a href="/about-us">About Us</a>
+                      <a href="/about-us">About Us</a>
                     </li>
                     <li>
-                    <a href="/privacy-policy">privacy policy</a>
+                      <a href="/privacy-policy">privacy policy</a>
                     </li>
                     <li>
-                    <a href="/contact">contact</a>
+                      <a href="/contact">contact</a>
                     </li>
                   </ul>
                 </nav>
@@ -303,38 +162,7 @@ const NavBar = () => {
           </div>
         </div>
       </div>
-      {/* <div className="header-bottom-furniture wrapper-padding-2 border-top-3">
-        <div className="container-fluid">
-          <div className="furniture-bottom-wrapper">
-            <div className="furniture-search">
-              <form action="#">
-                <input placeholder="I am Searching for . . ." type="text" />
-                <button>
-                  <i className="ti-search"></i>
-                </button>
-              </form>
-            </div>
-            <div className="furniture-wishlist">
-              <ul>
-                <li>
-                  <a
-                    data-bs-toggle="modal"
-                    data-target="#exampleCompare"
-                    href="#"
-                  >
-                    <i className="ti-reload"></i> Compare
-                  </a>
-                </li>
-                <li>
-                  <a href="wishlist.html">
-                    <i className="ti-heart"></i> Wishlist
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div> */}
+     
     </div>
   );
 };
