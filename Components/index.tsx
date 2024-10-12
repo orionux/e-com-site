3cbbcdaefd366b0ace0b478f6a382342769f7875
tj <thinupavani@gmail.com>
2024-10-12 12:39:51 +0530

    update

--- Components/index.tsx
+++ Components/index.tsx
@@ -8,8 +8,8 @@ import Head from 'next/head';
 import Script from 'next/script';
 
 type LayoutProps = {
-    children: React.ReactNode;
-  };
+  children: React.ReactNode;
+};
 
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
@@ -18,32 +18,32 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   //   const handleBeforeUnload = () => {
   //     localStorage.removeItem('authToken');
   //   };
-  
+
   //   window.addEventListener('beforeunload', handleBeforeUnload);
-  
+
   //   return () => {
   //     window.removeEventListener('beforeunload', handleBeforeUnload);
   //   };
   // }, []);
-  
+
   return (
     <>
       <NavBar />
       <main className='layout-margin'>{children}</main>
       <Footer />
-        <Script type="text/javascript" src="/assets/js/vendor/modernizr-3.11.7.min.js"/>
-        <Script type="text/javascript" src="/assets/js/vendor/jquery-1.12.4.min.js"/>
-        <Script type="text/javascript" src="/assets/js/popper.js"/>
-        <Script type="text/javascript" src="/assets/js/bootstrap.min.js"/>
-        <Script type="text/javascript" src="/assets/js/jquery.magnific-popup.min.js"/>
-        <Script type="text/javascript" src="/assets/js/isotope.pkgd.min.js"/>
-        <Script type="text/javascript" src="/assets/js/imagesloaded.pkgd.min.js"/>
-        <Script type="text/javascript" src="/assets/js/jquery.counterup.min.js"/>
-        <Script type="text/javascript" src="/assets/js/waypoints.min.js"/>
-        <Script type="text/javascript" src="/assets/js/ajax-mail.js"/>
-        <Script type="text/javascript" src="/assets/js/owl.carousel.min.js"/>
-        <Script type="text/javascript"src="/assets/js/plugins.js"/>
-        <Script type="text/javascript" src="/assets/js/main.js"/>
+      <Script type="text/javascript" src="/assets/js/vendor/jquery-1.12.4.min.js" />
+      <Script type="text/javascript" src="/assets/js/vendor/modernizr-3.11.7.min.js" />
+      <Script type="text/javascript" src="/assets/js/popper.js" />
+      <Script type="text/javascript" src="/assets/js/bootstrap.min.js" />
+      <Script type="text/javascript" src="/assets/js/jquery.magnific-popup.min.js" />
+      <Script type="text/javascript" src="/assets/js/isotope.pkgd.min.js" />
+      <Script type="text/javascript" src="/assets/js/imagesloaded.pkgd.min.js" />
+      <Script type="text/javascript" src="/assets/js/jquery.counterup.min.js" />
+      <Script type="text/javascript" src="/assets/js/waypoints.min.js" />
+      <Script type="text/javascript" src="/assets/js/ajax-mail.js" />
+      <Script type="text/javascript" src="/assets/js/owl.carousel.min.js" />
+      <Script type="text/javascript" src="/assets/js/plugins.js" />
+      <Script type="text/javascript" src="/assets/js/main.js" />
     </>
   );
 };
